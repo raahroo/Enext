b7e9d3dab9bd11dce86a16aafa0b0ecb146c7120
Raissa <raissaraahroo@gmail.com>
2016-07-04 17:16:18 -0300

    Add files via upload

---
+++ dist/js/carrossel.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(function(){
+	  $("#carrossel").jCarouselLite({
+			btnPrev : '.prev',
+			btnNext : '.next',
+			auto    : 4000,
+			speed   : 2000,
+			visible : 6
+	  })
+})
